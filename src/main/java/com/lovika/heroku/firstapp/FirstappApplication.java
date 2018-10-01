21a8dc29cb5fde2b43a45c8ca186090d14403d07
Lovika Saxena <Lovika_Saxena@external.mckinsey.com>
2018-10-01 16:15:11 +0530

    Lovika - First Commit

---
+++ src/main/java/com/lovika/heroku/firstapp/FirstappApplication.java
@@ -0,0 +1,14 @@
+package com.lovika.heroku.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@Configuration
+//@EnableAutoConfiguration
+@SpringBootApplication
+public class FirstappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstappApplication.class, args);
+    }
+}
---
+++ src/main/java/com/lovika/heroku/firstapp/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.lovika.heroku.firstapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class FirstController {
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String firstEndPoint(@RequestParam(value = "name", defaultValue = "User") String name) {
+        return "Hello " + name + " !";
+
+    }
+}
